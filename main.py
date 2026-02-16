e73a18940b73c0abf384da81c574ce9828ecac9b
Xurshidbek003 <xurshidbekt571@gmail.com>
2026-02-16 10:18:02 +0500

    CORS settings

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app = FastAPI(
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.FRONTEND_URL, "http://localhost:3000", "http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
